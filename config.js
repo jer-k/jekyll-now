7cd0506f53a3803f5fcd6086fd570b1cc7fd5dbf
jer-k <jeremyk112@gmail.com>
2020-12-14 20:08:38 -0800

    Remove pathprefix

--- config.js
+++ config.js
@@ -1,44 +1,45 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://jer-k.github.io',
-  pathPrefix: '/',
+  url: "https://jer-k.github.io",
   title: "Jeremy Kreutzbender's Blog",
-  subtitle: 'Welcome to my programming blog!',
-  copyright: '© 2020, Jeremy Kreutzbender. All rights reserved.',
-  disqusShortname: 'github-io-7',
+  subtitle: "Welcome to my programming blog!",
+  copyright: "© 2020, Jeremy Kreutzbender. All rights reserved.",
+  disqusShortname: "github-io-7",
   postsPerPage: 10,
-  googleAnalyticsId: 'UA-114514474-1',
+  googleAnalyticsId: "UA-114514474-1",
   useKatex: false,
   menu: [
     {
-      label: 'Programming Articles',
-      path: '/'
+      label: "Programming Articles",
+      path: "/",
     },
     {
-      label: 'Thinking Out Loud',
-      path: '/thinking'
+      label: "Thinking Out Loud",
+      path: "/thinking",
     },
     {
-      label: 'About Me',
-      path: '/about'
+      label: "About Me",
+      path: "/about",
     },
     {
-      label: 'Contact Me',
-      path: '/contact'
-    }
+      label: "Contact Me",
+      path: "/contact",
+    },
   ],
   author: {
-    name: 'Jeremy Kreutzbender',
-    photo: 'https://avatars2.githubusercontent.com/u/1280586?s=400&u=9b55d0854cc80b154f77741d40c06c99b5ba8b9e&v=4',
-    bio: "Hello I'm Jeremy. You can find me around the internet as j_kreutzbender or jer-k. I'm mostly thinking about Ruby & Rails, Elixir & Phoenix, Docker, and DevOps topics when it comes to programming.",
+    name: "Jeremy Kreutzbender",
+    photo:
+      "https://avatars2.githubusercontent.com/u/1280586?s=400&u=9b55d0854cc80b154f77741d40c06c99b5ba8b9e&v=4",
+    bio:
+      "Hello I'm Jeremy. You can find me around the internet as j_kreutzbender or jer-k. I'm mostly thinking about Ruby & Rails, Elixir & Phoenix, Docker, and DevOps topics when it comes to programming.",
     contacts: {
-      email: 'jeremy.kreutzbender@gmail.com',
-      twitter: 'j_kreutzbender',
-      github: 'jer-k',
-      linkedin: 'jeremykreutzbender',
-      instagram: 'j_kreutzbender',
-      stackoverflow: 'thejer'
-    }
-  }
+      email: "jeremy.kreutzbender@gmail.com",
+      twitter: "j_kreutzbender",
+      github: "jer-k",
+      linkedin: "jeremykreutzbender",
+      instagram: "j_kreutzbender",
+      stackoverflow: "thejer",
+    },
+  },
 };
